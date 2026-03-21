fc917cc0a3693c30c0249e58affe4e168b4600ac
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-03-21 08:12:01 +0100

    libs, setup

---
+++ lib/tsconf.ts
@@ -0,0 +1,4 @@
+/** @todo: find out why this is not importable with "@/root"! */
+import tsConf from "../tsconfig.json";
+
+export default tsConf;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import vikeSolid from "vike-solid/vite";
-import vike from "vike/plugin";
-import { defineConfig } from "vite";
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import vike from 'vike/plugin'
+import vikeSolid from 'vike-solid/vite'
+import { defineConfig } from 'vite'
+import tsConf from './lib/tsconf'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const pathAliases = Object.entries(tsConf.compilerOptions.paths).map(([key, [value]]) => {
+  if (key.includes('*')) {
+    const find = new RegExp(`^${key.replace('/*', '/(.*)$')}`)
+    const replacement = path.resolve(__dirname, value.replace('/*', '/$1'))
+    return { find, replacement }
+  }
+  return { find: key, replacement: path.resolve(__dirname, value) }
+})
 
 export default defineConfig({
   plugins: [vike(), vikeSolid()],
-});
+  resolve: {
+    alias: [...pathAliases],
+  },
+  // build: {
+  //   cssCodeSplit: false,
+  // },
+  server: {
+    port: 5555,
+  },
+  preview: {
+    port: 5556,
+  },
+})
