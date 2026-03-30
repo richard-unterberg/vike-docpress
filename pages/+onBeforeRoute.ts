f74d3605ebc0197ba18919286582387d4982a161
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-03-30 21:40:31 +0200

    fix route

--- pages/+onBeforeRoute.ts
+++ pages/+onBeforeRoute.ts
@@ -9,6 +9,10 @@ import { DEFAULT_LOCALE } from '@/lib/i18n/config'
 import { hasLocalePrefix, localizeHref, stripLocaleFromPathname } from '@/lib/i18n/routing'
 import { getStoredLocalePreference } from '@/lib/settings-store'
 
+const isPageContextRequest = (url: string) => {
+  return url.split('?')[0]?.endsWith('.pageContext.json') ?? false
+}
+
 const getCanonicalDocPathname = (pathname: string) => {
   const resolved = resolveHeadingByHrefPathname(pathname)
   return resolved ? getDocPath(resolved.docPath) : pathname
@@ -20,6 +24,7 @@ const getDocSlugFromPathname = (pathname: string) => {
 }
 
 const onBeforeRoute = (pageContext: PageContext) => {
+  const pageContextRequest = isPageContextRequest(pageContext.urlOriginal)
   const urlPathnameLocalized = pageContext.urlParsed.pathname
   const urlHasExplicitLocale = hasLocalePrefix(urlPathnameLocalized)
   const { locale, pathname } = stripLocaleFromPathname(urlPathnameLocalized)
@@ -27,11 +32,18 @@ const onBeforeRoute = (pageContext: PageContext) => {
   const docSlug = getDocSlugFromPathname(pathnameCanonical)
   const localePreference = !urlHasExplicitLocale ? getStoredLocalePreference() : null
 
-  if (urlHasExplicitLocale && locale !== DEFAULT_LOCALE && docSlug && !hasDocPageForLocale(docSlug, locale)) {
+  if (
+    !pageContextRequest &&
+    urlHasExplicitLocale &&
+    locale !== DEFAULT_LOCALE &&
+    docSlug &&
+    !hasDocPageForLocale(docSlug, locale)
+  ) {
     throw redirect(modifyUrl(pageContext.urlOriginal, { pathname: localizeHref(pathnameCanonical, DEFAULT_LOCALE) }))
   }
 
   if (
+    !pageContextRequest &&
     typeof window !== 'undefined' &&
     !urlHasExplicitLocale &&
     localePreference &&
@@ -41,7 +53,7 @@ const onBeforeRoute = (pageContext: PageContext) => {
     throw redirect(modifyUrl(pageContext.urlOriginal, { pathname: localizeHref(pathnameCanonical, localePreference) }))
   }
 
-  if (pathnameCanonical !== pathname) {
+  if (!pageContextRequest && pathnameCanonical !== pathname) {
     throw redirect(modifyUrl(pageContext.urlOriginal, { pathname: localizeHref(pathnameCanonical, locale) }))
   }
 
