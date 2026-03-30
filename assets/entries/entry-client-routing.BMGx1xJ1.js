6c27290b33ba3f24462440a3bfc6d2c3d78937c8
richard-unterberg <richard-unterberg@users.noreply.github.com>
2026-03-30 17:06:09 +0000

    deploy: 5f5b17bbb9598999db44a58e87c60dc75d373c35

--- assets/entries/entry-client-routing.BMGx1xJ1.js
+++
@@ -1,1 +0,0 @@
-import{o as t,a2 as i}from"../chunks/chunk-DF583pNb.js";import{i as n}from"../chunks/chunk-DX92mivV.js";t();i();n();
---
+++ assets/entries/entry-client-routing.DoF8XMx9.js
@@ -0,0 +1,1 @@
+import{q as t,a3 as i}from"../chunks/chunk-CHdDrvKt.js";import{i as n}from"../chunks/chunk-D-KwJLNj.js";t();i();n();
