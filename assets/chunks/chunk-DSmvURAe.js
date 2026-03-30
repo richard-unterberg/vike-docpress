9ec4de7f7a3c84a6d862db9b040910c89c69a9f5
richard-unterberg <richard-unterberg@users.noreply.github.com>
2026-03-30 17:41:13 +0000

    deploy: 690191521bc1c3ddcaaad9f4ddd575c181d48d37

---
+++ assets/chunks/chunk-DSmvURAe.js
@@ -0,0 +1,20 @@
+import{L as e,j as s}from"./chunk-CC3n88ML.js";const b=({type:t="info",heading:r,children:o})=>s.jsxs(n,{$variant:t,children:[r&&s.jsx(a,{children:r}),o]}),n=e.section.variants({base:`
+    p-4
+    mb-5
+    border
+    rounded-lg
+    prose-p:leading-7
+    prose-p:last:mb-0
+    prose-p:first:mt-0
+    prose-headings:first:mt-0
+    prose-headings:last:mb-0
+    prose-ul:first:mt-0
+    prose-ul:last:mb-0
+    prose-div:my-0!
+    text-sm
+  `,variants:{$variant:{info:"bg-info/5 border-info/30",warning:"bg-warning/5 border-warning/30",error:"bg-error/5 border-error/25",success:"bg-success/5 border-success/35"}},defaultVariants:{$variant:"info"}}),a=e.header`
+  mt-1
+  font-bold
+  text-base
+  mb-5
+`;export{b as A};
--- assets/entries/entry-client-routing.DoF8XMx9.js
+++
@@ -1,1 +0,0 @@
-import{q as t,a3 as i}from"../chunks/chunk-CHdDrvKt.js";import{i as n}from"../chunks/chunk-D-KwJLNj.js";t();i();n();
